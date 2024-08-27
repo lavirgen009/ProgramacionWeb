a95e8dc2b4adcc77be13d63a9acd523904a6c4ba
IVAN ESCOBAR <96350730+lavirgen009@users.noreply.github.com>
2024-08-26 18:39:08 -0600

    Tarea

---
+++ MiProgramaJS/app3.js
@@ -0,0 +1,13 @@
+
+
+let edad = prompt("Ingresa tu edad");
+console.log ("Tu edad es: " + edad + " Años");
+let peso = prompt("Ingresa tu peso");
+console.log ("Tu peso es: " + peso + " Kilos");
+let estatura = prompt("Ingresa tu estatura en metros");
+console.log ("Tu estatura es: " + estatura + " Metros");
+let nombre = prompt("Ingresa tu nombre");
+console.log ("Tu nombre es: " + nombre);
+let masacorporal =  parseInt( peso) / (parseFloat( estatura)*parseFloat( estatura)); 
+console.log ("Tu IMC es: " + masacorporal);
+
